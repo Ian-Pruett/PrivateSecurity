2d9d32b8c8109d25ef89ea5fcbd9cff504ff1756
Ian-Pruett <ianpruett14@gmail.com>
2019-02-23 20:14:44 -0800

    heroku maybe

--- cam_backend/users/routes.py
+++ cam_backend/users/routes.py
@@ -2,6 +2,7 @@ from flask import request, jsonify, Blueprint
 from cam_backend import db, bcrypt
 from cam_backend.models import User
 from geopy.geocoders import Nominatim
+from geopy.distance import geodesic, great_circle
 import urllib
 import certifi
 
@@ -13,7 +14,7 @@ def uo(args, **kwargs):
     return urllib.request.urlopen(args, cafile=certifi.where(), **kwargs)
 
 
-def convert_coordinates(addr):
+def geolocation(addr):
     geolocator = Nominatim()
     geolocator.urlopen = uo
     location = geolocator.geocode(addr)
@@ -26,11 +27,26 @@ def create():
     user = User(**request.json)
     pw_hash = bcrypt.generate_password_hash(user.password).decode('utf-8')
     user.password = pw_hash
-    user.latitude, user.longitude = convert_coordinates(user.address)
+    location = geolocation(user.address)
+    if location is None:
+        return jsonify({
+            'status': 0,
+            'validAddress': False,
+            'validPhoneNumber': True,
+            'validEmail': True,
+            'user': None
+            }), 200
+    user.latitude, user.longitude = location.latitude, location.longitude
     db.session.add(user)
     db.session.commit()
     db.session.refresh(user)
-    return jsonify({'status': 1}), 200
+    return jsonify({
+        'status': 1,
+        'validAddress': True,
+        'validPhoneNumber': True,
+        'validEmail': True,
+        'user': jsonify(row_to_user(user))
+        }), 200
 
 
 @users.route('/login', methods=['GET', 'POST'])
@@ -41,12 +57,7 @@ def login():
     password = r.get('password')
     query = User.query.filter_by(email=email).first()
     if bcrypt.check_password_hash(query.password, password):
-        return jsonify({
-            'status': 1, 
-            'firstName': query.firstName, 
-            'lastName': query.lastName,
-            'email': query.email
-        }), 200
+        return jsonify(row_to_user(query)), 200
     else:
         return jsonify({'status': 2, 'user': None}), 200
 
@@ -55,3 +66,29 @@ def login():
 def update_location():
     print(request.json)
     r = request.json
+    user = User.query.get(r.id)
+    current = (r.latitude, r.longitude)
+    home = (user.latitude, user.longitude)  
+    dist = min(geodesic(current, home).miles, 
+        great_circle(current, home).miles)
+    if dist >= 1:
+        user.status = False
+    else:
+        user.status = True
+    db.session.add(user)
+    db.session.commit()
+    db.session.refresh(user)
+    return jsonify({'status': 1}), 200
+    
+     
+def row_to_user(row):
+    return {
+        'status': 1,
+        'id': row.id,
+        'device_registration': row.device_registration,
+        'email': row.email,
+        'address': row.address,
+        'phone_num': row.phone_num,
+        'firstName': row.firstName,
+        'lastName': row.lastName,
+    }
