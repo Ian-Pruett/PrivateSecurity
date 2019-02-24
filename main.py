7f03358d11abd7fda99e91c190f092a8da63b375
Ian-Pruett <ianpruett14@gmail.com>
2019-02-24 00:57:37 -0800

    added some azure stuff

---
+++ main.py
@@ -0,0 +1,32 @@
+import numpy as np
+import cognitive_face as CF
+import cv2
+
+'''
+
+Key 1: 2b52e26916e64eeba2703fed6e75c7a2
+Key 2: dd39248b082f444584ee69796ef5ba28
+
+'''
+
+key = ''
+vision_base_url = "https://westus.api.cognitive.microsoft.com/vision/v2.0/"
+
+analyze_url = vision_base_url + "analyze"
+
+
+
+
+if __name__ == '__main__':
+    while True:
+        cap = cv2.VideoCapture(0)
+        ret, frame = cap.read()
+
+        cv2.imshow('frame', frame)
+        cv2.imwrite('frame.jpg', frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
