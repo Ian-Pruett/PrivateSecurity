fcb44290fddde895e52a9972c55905af865e121b
Ian-Pruett <ianpruett14@gmail.com>
2019-02-24 04:25:22 -0800

    .

--- main.py
+++ main.py
@@ -4,13 +4,9 @@ import cv2
 
 '''
 
-Key 1: 2b52e26916e64eeba2703fed6e75c7a2
-
-Key 2: dd39248b082f444584ee69796ef5ba28
-
 '''
 
-key = 'dd39248b082f444584ee69796ef5ba28'
+key = ''
 CF.Key.set(key)
 vision_base_url = "https://westus.api.cognitive.microsoft.com/vision/v2.0/"
 CF.BaseUrl.set(vision_base_url)
